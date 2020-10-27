35d1fa4d858124c860fce6ca1f4e15fe755e6ee0
Kayla Saint <saviorseven11@gmail.com>
2020-10-27 16:05:54 -0700

    Fix God Apple effects

--- src/main/java/com/civlegacy/stamina/Listeners/PlayerListener.java
+++ src/main/java/com/civlegacy/stamina/Listeners/PlayerListener.java
@@ -3,8 +3,10 @@ package com.civlegacy.stamina.Listeners;
 import com.civlegacy.stamina.Stamina;
 import com.civlegacy.stamina.StaminaManager;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerItemConsumeEvent;
@@ -12,6 +14,10 @@ import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import java.util.Collection;
 
 public class PlayerListener implements Listener {
 
@@ -33,11 +39,21 @@ public class PlayerListener implements Listener {
         ItemMeta stamMeta = stamina.getItemMeta();
         stamMeta.setDisplayName("Stamina");
         stamina.setItemMeta(stamMeta);
+        Player player = event.getPlayer();
         PlayerInventory playerInventory = event.getPlayer().getInventory();
 
         if(event.getItem().isSimilar(stamina)) {
             manager.givePlayerStamina(event.getPlayer(), 1);
             event.getPlayer().sendMessage(ChatColor.AQUA + "You now have " + ChatColor.GREEN + manager.getPlayerStamina(event.getPlayer()) + ChatColor.AQUA + " stamina.");
+            Bukkit.getScheduler().runTaskLater(Stamina.getPlugin(), new Runnable() {
+                @Override
+                public void run() {
+                    Collection<PotionEffect> potionEffects = player.getActivePotionEffects();
+                    for(PotionEffect potionEffect : potionEffects) {
+                        player.removePotionEffect(potionEffect.getType());
+                    }
+                }
+            }, 10);
         }
     }
 }
